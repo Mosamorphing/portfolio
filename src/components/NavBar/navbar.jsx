9defc4463ec37742445cff24d69d8dfc7f329acb
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-22 23:56:27 +0100

    added a hamburger to the navbar

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../../public/logo.png';
 import { Link } from 'react-scroll';
+import menu from '../../../public/menu.png'
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
       <img src={logo} alt="logo" className='logo' />
       <div className='desktopMenu'>
-        <Link activeClass='active' to='Home' spy={true} smooth = {true} offset={-50} duration={500} className="desktopMenuListItem">Home</Link>
-        <Link activeClass='active' to='Portfolio' spy={true} smooth = {true} offset={-100} duration={500}className="desktopMenuListItem">Portfolio</Link>
+        <Link activeClass='active' to='Home' spy={true} smooth = {true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
+        <Link activeClass='active' to='Portfolio' spy={true} smooth = {true} offset={-50} duration={500}className="desktopMenuListItem">Portfolio</Link>
         <Link activeClass='active' to='Publications' spy={true} smooth = {true} offset={-100} duration={500}className="desktopMenuListItem">Publications</Link>
       </div>
       <a href="mailto:mosadoluwamorphing@gmail.com" className='desktopMenuBtn'>
         Send a Mail
       </a>       
+
+      <img src={menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
+      <div className="navMenu" style={{display: showMenu? 'flex': 'none'}}>
+        <Link activeClass='active' to='Home' spy={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link activeClass='active' to='Portfolio' spy={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+        <Link activeClass='active' to='Publications' spy={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Publications</Link>
+      </div>
     </nav>
   );
 };
