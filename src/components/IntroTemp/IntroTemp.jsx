a6bd54d604ae8d7000536c9a2e9728690782a86d
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-22 22:29:01 +0100

    deployment issues finally resolved! issue: it was a case-sensitive issue from the naming of one if my components 'Intro' solution: i deleted the component and rewrote the code. This time, paying attention to the naming of both the folder and files within, and the way they are written when imported.

--- src/components/IntroTemp/IntroTemp.jsx
+++ src/components/IntroTemp/IntroTemp.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './introTemp.css';
 import bg from '../../../public/image.png';
+import wave from '../../../public/wave.png'; 
 import {Link} from 'react-scroll';
-import btnImg from '../../../public/hireme.png';
+// import btnImg from '../../../public/hireme.png';
 
 
 export const IntroTemp = () => {
   return (
     <section id="intro">
         <div className="introContent">
-            <span className ='hello'> Hello there!  </span>
-            <span className='introText'>I'm <span className='introName'>Mosa</span> <br/> Technical PM — Design & <br/> Product Thinking </span>
-        <p className="introPara">Focused on transforming concepts into successful, user-centered products</p>
-        <Link><button className="btn"><img src={btnImg} alt="" className='btnImg'/> Open to collaborations </button></Link>
+        <span className='hello'> 
+          Hello there! <img src={wave} alt="wave" className="waveIcon" />
+        </span>
+            <span className='introText'>The name is <span className='introName'>Mosadoluwa</span></span>
+            <span className='introText2'>Adventurer, Thinker, Doer.</span>
+            <span className='introText3'>As a Technical Product Manager, <br/> <span className='introName2'>I'm at the intersection of people, products, and profits.</span></span>
+        <p className="introPara">Below are some of my current engagements.
+        </p>
+        {/* <Link><button className="btn"><img src={btnImg} alt="" className='btnImg'/> Open to collaborations </button></Link> */}
         </div>
         <img src={bg} alt="profile" className="bg" />
     </section>
