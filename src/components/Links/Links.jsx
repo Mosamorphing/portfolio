df5fe5a02dbeb5d8cd7bc0e8b0d32d62b2132ec0
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-10-22 22:42:53 +0100

    cleaning my code

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './links.css'; // Import the CSS file for styling
+import './links.css'; 
 
 export const Links = () => {
   return (
-    <div className="links-container"> {/* Add a wrapper div */}
+    <div className="links-container"> 
       <div className="links">
       <a href="https://x.com/mofasasi" target="_blank" rel="noopener noreferrer">X/Twitter   </a>
       <a href="https://github.com/Mosamorphing" target="_blank" rel="noopener noreferrer">GitHub</a>
--- src/components/Projectcard/ProjectCard.jsx
+++ src/components/Projectcard/ProjectCard.jsx
@@ -1,24 +1,9 @@
-// import React from 'react';
-// import './projectcard.css'; // Import CSS specific to ProjectCard component
-
-// export const ProjectCard = ({ logo, name, description, logoStyle }) => {
-//   return (
-//     <div className="project-card">
-//       <img src={logo} alt={name} style={logoStyle}/>
-//       <h3>{name}</h3>
-//       <p>{description}</p>
-//     </div>
-//   );
-// };
-
-// export default ProjectCard;
-
 import React from 'react';
-import './projectcard.css'; // Import CSS specific to ProjectCard component
+import './projectcard.css'; 
 
 export const ProjectCard = ({ logo, name, description, logoStyle, url }) => {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="project-card"> {/* Wrap in an <a> tag */}
+    <a href={url} target="_blank" rel="noopener noreferrer" className="project-card"> 
       <img src={logo} alt={name} style={logoStyle} />
       <h3>{name}</h3>
       <p>{description}</p>
